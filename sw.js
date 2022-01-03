a9973acafe034c4743ebeb8159d1d910a4b77b50
Qasim <69038179+Qasim345@users.noreply.github.com>
2022-01-03 08:02:34 +0430

    update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("static").then(cache => {
-      return cache.addAll(["/", "dist/style.css","dist/main.js", "dist/logo.png","dist/material-icons.ttf"]);
+      return cache.addAll(["/", "dist/style.css", "dist/logo.png"]);
     })
   );
 });
