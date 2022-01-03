57e88af6c34430e91da81deb0ba15bfce17f1edf
Qasim <69038179+Qasim345@users.noreply.github.com>
2022-01-03 07:45:32 +0430

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("static").then(cache => {
+      return cache.addAll(["/", "dist/style.css", "dist/typed.js", "dist/jquery.min.js", "dist/logo.png", "dist/king.png", "dist/material-icons.ttf", "dist/ProSans.ttf"]);
+    })
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respndWith(
+    catches.match(e.request).then(respons => {
+      return respons || fetch(e.request);
+    })
+  );
+});
